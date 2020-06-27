bb0f3ce5688ff76b721857870c5e5c72788c7fe0
PiotrGasiorek <piotrjgasiorek@gmail.com>
2020-06-27 02:47:15 +0200

    Created folder for components

---
+++ src/components/layout/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="@/assets/logo.svg" alt="">
+        </router-link>
+        <div>
+            <router-link to="/features">Features</router-link>
+            <br>
+            <router-link to="/board">Board</router-link>
+            <br>
+            <router-link to="/contact">Contact</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+}
+</script>
+
+<style scoped lang='sass'>
+nav
+    width: 100%
+    display: flex
+    height: 50px
+    align-items: center
+
+    div
+        display: flex
+        margin-left: auto
+
+        *
+            padding: 0 0 0 20px
+</style>
