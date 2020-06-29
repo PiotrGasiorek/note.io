b90104328784e9843182311f53e23101ea0f1050
PiotrGasiorek <piotrjgasiorek@gmail.com>
2020-06-29 15:13:50 +0200

    Created homepage structure and styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <header class="header home">
+    <div class="row">
+      <div class="">    
+        <h1 class='title title--lg'>
+          Best app for taking notes and keeping your thoughts in order
+        </h1>
+        <router-link to="/board" class='btn'>Start for free</router-link>
+      </div>
+      <div class="">
+        <img :src='image' alt="">
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Marcus_Aurelius from '@/assets/Marcus_Aurelius.png'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+
+  export default {
+    name: 'Home',
+    data: function () {
+        return {
+            image: Marcus_Aurelius
+        }
+    }
   }
-}
 </script>
+
+
+<style scoped lang='sass'>
+  .home
+    align-items: flex-end
+    display: flex
+
+  .row
+    display: grid
+    grid-template-columns: repeat(2, 1fr)
+    grid-gap: $margins--md
+  
+  img
+    max-width: 40vw
+    position: absolute
+    right: -100px
+    top: 100px
+    opacity: .7
+
+    &:hover
+      opacity: 1
+      max-width: calc(40vw + 50px)
+</style>
