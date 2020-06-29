ef59ce69e1a198e63ea9f96e12b52b591e58123c
PiotrGasiorek <piotrjgasiorek@gmail.com>
2020-06-29 15:10:22 +0200

    Added styles functionality validation etc

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,9 +1,44 @@
 <template>
-    <p>Single note</p>
+    <div class='note'>
+        <h6 class='note__title'>{{ note.title }}</h6>
+        <div class="note__details">
+            <p className='note__time'>{{ note.time }}</p>
+            <p className='note__date'>{{ note.date }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'Note',
+        props: ['note']
     }
 </script>
+
+<style scoped lang='sass'>
+    .note
+        width: 100%
+        display: flex
+        height: 60px
+        align-items: center
+        font-family: $font-families--secondary
+        font-weight: $font-weights--regular
+
+        &__title
+            font-size: 18px
+        &__details
+            display: flex
+            align-items: center
+            width: 200px
+            margin-left: auto
+            color: $colors--tertiary
+            font-size: 16px
+
+            *
+                width: 50%
+
+        &__date
+            text-align: right
+            margin-left: auto
+
+</style>
